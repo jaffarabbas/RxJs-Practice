46d6b371788ebd9cf1024152ae9cb917444c2a3b
Jaffar Abbas <gamaportal8@gmail.com>
2023-01-21 09:37:15 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,43 @@
 const { Observable } = require('rxjs');
+const { map } = require('rxjs/operators');
+
+const users = {
+    data: [
+        {
+            status: 'active',
+            age: 15
+        },
+        {
+            status: 'active',
+            age: 20
+        },
+        {
+            status: 'inactive',
+            age: 25
+        },
+    ]
+}
 
 const observable = new Observable((subscribe) => {
-    subscribe.next(10);
-    subscribe.next(11);
-    subscribe.next(20);
-    subscribe.next(30);
-    subscribe.next(40);
-})
+    subscribe.next(users);
+}).pipe(
+    map((users) => {
+        console.log('1) users', users)
+        return users.data;
+    }),
+    map((users) => {
+        console.log('2) users', users)
+        return users.filter((user) => {
+            return user.status === 'active'
+        });
+    }),
+    map((users) => {
+        console.log('3) users', users)
+        return (users.reduce((acc, user) => {
+            return acc + user.age
+        }, 0)) / users.length
+    })
+);
 
 const observer = {
     next: (value) => {
